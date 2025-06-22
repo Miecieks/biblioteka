29c9038388752e2a17ef84f2d2c74e4a5fc50d46
Mieciek <mateusz.muzyka@katowice.zdz.katowice.pl>
2025-06-22 21:22:03 +0200

    qq

--- baza/baza.sql
+++ baza/baza.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (
   user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(25),
+  last_name VARCHAR(50),
   user_name VARCHAR(50) NOT NULL UNIQUE,
   password VARCHAR(100),
   email VARCHAR(100) UNIQUE,
-  admin BOOLEAN
+  admin BOOLEAN,
+  user_inner_id VARCHAR(8),
+  pfp VARCHAR(35)
 );
 
 INSERT INTO users (user_name,password,email,admin) 
