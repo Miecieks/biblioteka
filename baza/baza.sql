6cfe7cab0341e42ff957629c7aed2a1d29aeec7f
Miecieks <mateuszmuzyka7@gmail.com>
2025-07-30 22:39:38 +0200

    git

--- baza/baza.sql
+++ baza/baza.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS users (
-  user_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Users (
+  id SERIAL PRIMARY KEY,
   first_name VARCHAR(25),
   last_name VARCHAR(50),
   user_name VARCHAR(50) NOT NULL UNIQUE,
@@ -10,5 +10,36 @@ CREATE TABLE IF NOT EXISTS users (
   pfp VARCHAR(35)
 );
 
-INSERT INTO users (user_name,password,email,admin) 
-VALUES ('admin','ZAQ!2wsx','admin@op.pl',true);
+CREATE TABLE IF NOT EXISTS Libraries (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(60) NOT NULL,
+  location VARCHAR(60) NOT NULL,
+  penalty_per_day FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Books (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(70) NOT NULL,
+  author VARCHAR(80),
+  price FLOAT,
+  genre VARCHAR(15),
+  library_id INT NOT NULL,
+  is_avaible BOOLEAN,
+  CONSTRAINT fk_books FOREIGN KEY (library_id)
+  REFERENCES Libraries(id)
+);
+CREATE TABLE IF NOT EXISTS Rented (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  book_id INT NOT NULL,
+  to_return DATE,
+  penalty FLOAT,
+  is_extended BOOLEAN,
+  CONSTRAINT fk_books2 FOREIGN KEY (book_id)
+  REFERENCES Books(id) ,
+  CONSTRAINT fk_users2 FOREIGN KEY (user_id)
+  REFERENCES Users(id)
+);
+INSERT INTO users (first_name,last_name,user_name,password,email,admin,user_inner_id,pfp) 
+VALUES ('Jan','Nowak','admin','ZAQ!2wsx','admin@op.pl',true,'00000000','profiles/admin.png');
+
--- golang/main.go
+++ golang/main.go
@@ -5,21 +5,123 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 )
 
-func main() {
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "qqqq")
+}
+
+func initialize(query string) ([]interface{}, error) {
 	ctx := context.Background()
 	conn, err := pgx.Connect(ctx, passes.Certs("nazwa"))
 	if err != nil {
-		log.Fatalf("Błąd połączenia z bazą: %v", err)
+		return nil, fmt.Errorf("błąd połączenia z bazą: %v", err)
 	}
 	defer conn.Close(ctx)
-	var currentTime string
-	err = conn.QueryRow(ctx, "SELECT user_name FROM users").Scan(&currentTime)
+
+	rows, err := conn.Query(ctx, query)
 	if err != nil {
-		log.Fatalf("Błąd zapytania: %v", err)
+		return nil, fmt.Errorf("błąd zapytania: %v", err)
 	}
-	fmt.Println("Aktualny czas z bazy:", currentTime)
+	defer rows.Close()
+
+	var results []interface{}
+	for rows.Next() {
+		values, err := rows.Values()
+		if err != nil {
+			return nil, fmt.Errorf("błąd pobierania wartości: %v", err)
+		}
+		results = append(results, values...)
+	}
+	if rows.Err() != nil {
+		return nil, fmt.Errorf("błąd podczas iteracji po wierszach: %v", rows.Err())
+	}
+
+	return results, nil
+}
+func main() {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8085"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.GET("/ping", func(c *gin.Context) {
+		ping_test, err := initialize("SELECT user_name FROM users")
+		if err != nil {
+			log.Printf("Błąd podczas wykonywania zapytania: %v", err)
+		}
+		c.JSON(200, gin.H{
+			"message": ping_test,
+		})
+	})
+	router.GET("api/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		action = action[1:]
+		if action == "" {
+			c.JSON(400, gin.H{
+				"message": "Brak parametru",
+			})
+		} else {
+			switch action {
+			case "get":
+				query := "SELECT * FROM users WHERE user_name = '" + name + "'"
+				qqq, err := initialize(query)
+				if err != nil {
+					log.Printf("Błąd podczas wykonywania zapytania: %v", err)
+				}
+				c.JSON(200, gin.H{
+					"veryf":     "true",
+					"FirstName": qqq[1],
+					"LastName":  qqq[2],
+					"pfp":       qqq[8],
+				})
+			}
+		}
+	})
+
+	type LoginStruct struct {
+		Login string `json:"login"`
+		Pass  string `json:"pass"`
+	}
+
+	router.POST("api/user/:action", func(c *gin.Context) {
+		var r LoginStruct
+		action := c.Param("action")
+		if action == "" {
+			c.JSON(400, gin.H{
+				"message": "Brak parametru",
+			})
+		} else {
+
+			switch action {
+			case "verify":
+				if err := c.BindJSON(&r); err != nil {
+					c.JSON(400, gin.H{"success": false, "res": "Niepoprawny JSON"})
+					return
+				}
+				query := "SELECT * FROM users WHERE user_name = '" + r.Login + "' AND password = '" + r.Pass + "'"
+				qqq, err := initialize(query)
+				if err != nil {
+					log.Printf("Błąd podczas wykonywania zapytania: %v", err)
+				}
+				c.JSON(200, gin.H{"success": true, "message": "Zalogowano!", "id": qqq[0]})
+
+			}
+
+		}
+	})
+
+	router.Run()
 }
