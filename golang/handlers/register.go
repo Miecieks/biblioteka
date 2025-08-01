c6016a7325f48d46c475f44d8bfc97473206b7d4
Miecieks <mateuszmuzyka7@gmail.com>
2025-08-02 01:49:44 +0200

    git2

---
+++ golang/handlers/register.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	passes "biblioteka-backend/packages"
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+)
+
+func Register() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.Background()
+		conn, err := pgx.Connect(ctx, passes.Certs("nazwa"))
+		if err != nil {
+			fmt.Errorf("błąd połączenia z bazą: %v", err)
+		}
+		defer conn.Close(ctx)
+		c.JSON(http.StatusOK, gin.H{"succeded": true, "message": "zarejestrowano"})
+	}
+}
